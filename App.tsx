92030b273ce28e5ec7b43d0492cfdcec9e611a05
Isaac Poole <isaacpoolemusic@gmail.com>
2025-09-19 09:51:52 -0400

    sets up shadcn ui

--- App.tsx
+++ App.tsx
@@ -12,9 +12,9 @@ import {
   View,
 } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
+  SafeAreaProvider
 } from 'react-native-safe-area-context';
+import { PortalHost } from '@rn-primitives/portal';
 
 import "./global.css"
 
@@ -25,13 +25,12 @@ function App() {
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
+      <PortalHost />
     </SafeAreaProvider>
   );
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
