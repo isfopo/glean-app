327761492bd031edbcce1edeef42b4024942a965
Isaac Poole <isaacpoolemusic@gmail.com>
2025-09-19 00:59:41 -0400

    feat: Setup Nativewind and Tailwind configuration
    This commit integrates Nativewind and Tailwind CSS into the project for
    styling React Native components. It includes:
    
    - Installation of necessary dependencies. - Configuration of Babel to
    enable Nativewind. - Creation of a global.css file for Tailwind
    directives. - Setup of Tailwind configuration file. - Update of App.tsx
    to use Tailwind classes. - Modified Podfile.lock for required
    dependencies.

--- App.tsx
+++ App.tsx
@@ -7,7 +7,6 @@
 
 import {
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
@@ -16,6 +15,8 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
+import "./global.css"
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -32,16 +33,12 @@ function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
   return (
-    <View style={styles.container}>
-      <Text>HI</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
